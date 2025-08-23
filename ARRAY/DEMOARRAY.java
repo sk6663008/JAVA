9722134e944adbce92cef025f43b36baf5f05c5e
sk6663008 <sk6663008@gmail.com>
2025-08-23 23:47:27 +0530

    Create DEMOARRAY.java

---
+++ ARRAY/DEMOARRAY.java
@@ -0,0 +1,44 @@
+package Arrays_;
+
+import java.util.Scanner;
+
+public class demoarray {
+    public static void main(String []args){
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(add(sc.nextInt(), sc.nextInt()));
+
+        int [] ages = new int[5];
+        float[] weights = new float[5];
+        String[] names = new String[5];
+
+        ages[0]=10;
+        ages[1]=20;
+        ages[3]=33;
+
+        weights[0]= 10.22f;
+        weights[1]=12.4f;
+        weights[4]=33.56f;
+
+        names[0]="shashank";
+        names[2]="vikram";
+        names[3]="rahul";
+
+        System.out.println(ages[0]);
+        System.out.println(ages[3]);
+
+        System.out.println(weights[2]);
+        System.out.println(weights[3]);
+
+        System.out.println(names[2]);
+        System.out.println(names[4]);
+
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
+    }
+
+}
