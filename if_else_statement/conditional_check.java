cfb54b0010ef7a7c5ae282bd57839b76e781dbb5
sk6663008 <sk6663008@gmail.com>
2025-08-23 23:49:13 +0530

    Create conditional_check.java

---
+++ if_else_statement/conditional_check.java
@@ -0,0 +1,20 @@
+package if_else_statement;
+
+import java.util.Scanner;
+public class conditional_check {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number=");
+
+        int num = sc.nextInt();
+
+        if(num%2==0 && num%3==0){
+            System.out.println("even and not divisible by 3");
+        }else
+        {
+            System.out.println("odd or not divisible by 3");
+        }
+    }
+}
+
