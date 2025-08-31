f68ee71f10f1cb544b9fed238cc31d51d396a9d4
Shashank kumar <sk6663008@gmail.com>
2025-08-31 22:28:30 +0530

    Add files via upload

---
+++ ARRAY/pair_sum.java
@@ -0,0 +1,39 @@
+package Arrays_;
+
+import java.util.Scanner;
+
+public class pair_sum {
+    static int pairsum(int arr[], int target){
+        int n = arr.length;
+        int ans=0;
+
+        for(int i = 0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+               if( arr[i]+arr[j]==target){
+                   ans++;
+               }
+
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter size of array: ");
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            System.out.println("Enter the array elements:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println("Enter the target: ");
+           int target = sc.nextInt();
+
+        System.out.println(pairsum(arr, target));
+    }
+}
