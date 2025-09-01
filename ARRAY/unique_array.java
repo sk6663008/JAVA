2cac7f7d91ccbdab6f8c882d853a4227540e393e
Shashank kumar <sk6663008@gmail.com>
2025-09-01 21:42:29 +0530

    Add files via upload

---
+++ ARRAY/unique_array.java
@@ -0,0 +1,54 @@
+package Arrays_;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class unique_array {
+   public static void uniquearray(int arr[]){
+       int n = arr.length;
+       for (int i = 0; i < n; i++){
+           int count  = 0;
+           for (int j = 0; j < n; j++){
+               if (arr[j] == arr[i] && i!=j){
+                   count++;
+               }
+           }
+           if (count == 0){
+               System.out.println(arr[i]);
+           }
+       }
+    }
+    public static void unique(int[] arr){
+       int n = arr.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int j : arr) {
+            map.put(j, map.getOrDefault(j, 0) + 1);
+        }
+
+        for (int i = 0; i < n; i++){
+            if (map.get(arr[i])  ==  1){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take array size
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        // Create array
+        int[] arr = new int[n];
+
+        // Take input
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        unique(arr);
+    }
+}
