57b6cbb475a7dff2362eb46ff6dfb25442da230a
Shashank kumar <sk6663008@gmail.com>
2025-09-02 22:31:28 +0530

    Add files via upload

---
+++ ARRAY/repeating_array.java
@@ -0,0 +1,36 @@
+package Arrays_;
+
+import java.util.Scanner;
+
+public class repeating_array {
+    public static int repeating(int arr[]) {
+        int ans = -1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    return arr[i];
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take array size
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        // Create array
+        int[] arr = new int[n];
+
+        // Take input
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("first repeating no : " + repeating(arr));
+    }
+}
+
+
