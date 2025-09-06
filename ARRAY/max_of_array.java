757b391941a290bebcf913115e9ac122ccd1fc0f
Shashank kumar <sk6663008@gmail.com>
2025-09-06 22:37:28 +0530

    Update max_of_array.java

--- ARRAY/max_of_array.java
+++ ARRAY/max_of_array.java
@@ -4,7 +4,7 @@ public class max_of_array {
    static void maxarray(){
         int []arr = {3,5,9,2,7,6};
         int ans = 0;
-       for(int i=0;i<arr.length;i++){
+        for(int i=0;i<arr.length;i++){
            if(arr[i]> ans){
                ans = arr[i];
            }
@@ -16,3 +16,4 @@ public class max_of_array {
        maxarray();
     }
 }
+
